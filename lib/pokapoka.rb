c1b8fb21c2c68d5c4a978390dada3405c4ec450b
Adam Stankiewicz <sheerun@sher.pl>
2013-02-27 10:51:28 +0100

    Show preview in quick look

--- lib/pokapoka.rb
+++ lib/pokapoka.rb
@@ -1,29 +1,19 @@
 require "pokapoka/version"
 require "github/markdown"
-require 'rack'
 
 module Pokapoka
-  def self.app
-    tpl = '<html>
-    <head>
-    <link href="https://gist.github.com/assets/application-3e2433f6da817bf23a85587d46e5af5c.css" media="screen, print" rel="stylesheet" />
-    <style type="text/css">article{max-width:900px;margin:auto;padding:50px;border:1px solid #ccc;border-top:0;}</style>
-    </head>
-    <body><article class="markdown-body">BODY</atricle></body>
-    </html>'
+  def self.render(path = 'README.md')
+    template = File.read(File.expand_path('../pokapoka/template.html', __FILE__))
+    template = template.gsub(/__BODY__/, GitHub::Markdown.render_gfm(File.read(path)))
 
-    app = Proc.new do |env|
-      path = env["REQUEST_URI"].sub("/", "").strip
-      path = path == "" ? "README.md" : path
-      if File.exist?(path)
-        [200, {"Content-type" => "text/html"}, tpl.gsub("BODY", GitHub::Markdown.render_gfm(File.read(path))).each_line]
-      else
-        [404, {}, "stfu".each_line]
-      end
-    end
+    template
   end
 
-  def self.run!
-    ::Rack::Handler.default.run app, :Port => 9999
+  def self.html_path(path = 'README.md')
+    html = render(path)
+    file_path = '/tmp/' + File.basename(path, '.md') + '.html'
+    File.open(file_path, 'w') { |file| file.write(html) }
+
+    file_path
   end
 end
